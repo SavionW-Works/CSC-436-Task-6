2a15faf3114e86842760f88e0032b069682c5b20
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2023-03-10 12:31:51 -0500

    Added basis of each Book page

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -39,27 +39,108 @@ const Book = () => {
         {error && <ErrorAlert>{error}</ErrorAlert>}
         {!error && !loading &&
             <>
-                <div className="container columns-3 gap-5">
+
+
+                <div className="container">
+
                     <div className="row">
-                        <img src={book.imageURL} />
-                    </div> 
+                        <div className="col-3 text-center">
+                            <img className="img-thumbnail" src={book.imageURL} /> 
+                            <Link className='hover:underline text-2xl font-bold' to={`/books`}>Back to Listing</Link>
+                        </div>
 
-                    <div className="justify-content-left">
-                        <p className='text-2xl font-bold'> {book.title}</p> 
-                        <p className='text-lg'>By: {book.author}</p> 
-                        <p className='text-lg'>Publisher: {book.publisher}</p> 
-                        <p className='text-lg'>Year: {book.year}</p> 
-                        <p className='text-lg'>Pages: {book.pages}</p> 
-                        <p className='text-lg'>Country: {book.country}</p>
-                    </div>
-                    
-                    <div>
-                        <Link className='hover:underline text-2xl font-bold' to={`/books`}>Back to Listing</Link>
-                    </div>
-                    
-                </div> 
+                        {/* Content */}
+                        <div className="col-9 w-100">
+                            
+                            <div className="row-12 align-items-start pb-6">
+                                <div className="col align-items-left">
+                                    <p className="font-bold display-4"> {book.title} </p>
+                                    <p className="display-4 fs-3"> By: {book.author} </p>
+                                </div>
+                            </div>
 
-                
+                            <div className="row py-3">
+                                
+                                <div className="col-3">
+                                    <div class="card text-center">
+                                        <div className="card-header font-bold text-xl">
+                                            Publisher
+                                        </div>
+                                        <div class="card-body">
+                                            <h3 className='text-xl'>
+                                                {book.publisher}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                                <div className="col-3">
+                                    <div class="card text-center">
+                                        <div className="card-header font-bold text-xl">
+                                            Publish Date
+                                        </div>
+                                        <div class="card-body">
+                                            <h3 className='text-xl'>
+                                                {book.year}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                                <div className="col-3">
+                                    <div class="card text-center">
+                                        <div className="card-header font-bold text-xl">
+                                            Pages
+                                        </div>
+                                        <div class="card-body">
+                                            <h3 className='text-xl'>
+                                                {book.pages}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                                <div className="col-3">
+                                    <div class="card text-center">
+                                        <div className="card-header font-bold text-xl">
+                                            Country
+                                        </div>
+                                        <div class="card-body">
+                                            <h3 className='text-xl'>
+                                                {book.country}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+
+
+                </div>
+
+
+
+
+
+
+
+
+                <div>
+                    
+                </div>
+
+
+
+
 
             </>
         }
